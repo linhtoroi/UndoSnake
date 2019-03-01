51671cea87933c0f4ff5f063d84d61edf957556e
Linhtoroi <linhlinh.torres.reus@gmail.com>
2019-03-01 16:32:19 +0700

    Initial commit.

--- B1.cpp
+++ B1.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
 using namespace std;
-int factorial(int x)
-{
-    if (x>1)
-    {
-        cout<< "x = " << x << " at "<< &x << endl;
-        return x*factorial(x-1);
-    }
-    else
-    {
-        cout<< "x = " << x << " at " << &x << endl;
-        return 1;
-    }
-
-}
 int main()
 {
-    int N;
-    cin>>N;
-    cout<<factorial(N);
+    int c=10;
+    int a[3];
+    char b[3];
+    char v;
+    for (int i=0; i<3; i++)
+    {
+        cout<<&a[i]<<" ";   // các ô nhớ liên tiếp nhau, cách nhau 4 byte
+    }
+    cout<<endl;
+    for (int i=0; i<3; i++)
+    {
+        cout<<(void*)&b[i]<<" ";   // các ô nhớ liên tiếp cách nhau 1 byte
+    }
+    cout<<endl;
+    cout<<&c<<" "<<(void*)&v; // ô nhớ của kí tự int được lưu sau ô nhớ của 2 mảng, ô nhớ kí tự của chả được lưu trước ô nhớ của mảng char.
 }
--- B2.cpp
+++ B2.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 using namespace std;
+int factorial(int x)
+{
+    if (x>1)
+    {
+        cout<< "x = " << x << " at "<< &x << endl;
+        return x*factorial(x-1);
+    }
+    else
+    {
+        cout<< "x = " << x << " at " << &x << endl; // Chuỗi giá trị tăng dần; Các biến địa phương được lưu lần lượt và được giải phóng theo chiều ngược lại sau khi đã được thực hiện;
+        return 1;                                   // 0X28FEA0 - 0X28FE70 = 48; =>> kích thước của một stack frame cho hàm factorial là 6 byte.
+    }
+
+}
 int main()
 {
-    int a[10];
-    char b[10];
-    for (int i=0; i<10; i++)
-    {
-        cout<<&a[i]<<" ";
-    }
-    cout<<endl;
-    for (int i=0; i<10; i++)
-    {
-        cout<<&b[i]<<" ";
-    }
+    int N;
+    cin>>N;
+    cout<<factorial(N);
 }
